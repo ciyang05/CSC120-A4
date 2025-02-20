4c82bcd8b0036f2f1b71f82fc58db0bd1f5d0ac7
cyang05 <ciyang@smith.edu>
2025-02-19 20:40:25 -0500

    car

--- Car.java
+++ Car.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
+
 public class Car {
+
+    // Attributes 
+    private ArrayList <Passenger>  passengersOnboard;
+    private int maxCapacity;
+
+    public Car(int max){
+        this.maxCapacity = max;
+        this.passengersOnboard = new ArrayList<>();
+    }
 
 }

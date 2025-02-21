f1ae81ff65701b5e55d39afc6e9eb7d58910b2ab
cyang05 <ciyang@smith.edu>
2025-02-21 16:01:49 -0500

    Car class - almost finished

--- Car.java
+++ Car.java
@@ -8,8 +8,10 @@ public class Car {
     private int maxCapacity;
 
 
+
     /**
-     * 
+     * the constructor
+     * will initialize the maxCapcity and arraylist for passengers on baord
      * @param maxCapacity
      */
 
@@ -17,6 +19,7 @@ public class Car {
         this.maxCapacity = maxCapacity;
         this.passengersOnboard = new ArrayList<>();
     }
+
 
     /**
      * the getter for max capacity
@@ -27,6 +30,7 @@ public class Car {
         return this.maxCapacity;
     }
 
+
     /**
      * the getter for seatsRemaining method 
      * @return number of seats remaining 
@@ -36,10 +40,43 @@ public class Car {
         return seatsRemaining();
     }
 
-    public Passenger addPassenger(Passenger p) {
+
+    /**
+     * checks to see if car is full and passenger is already on board. if not, adds passenger to array list
+     * @param p
+     * @return false if maxCapacity is filled and passenger p is on board. otherwise, return true
+     */
+
+    public boolean addPassenger(Passenger p) {
+        int cap = passengersOnboard.size();
+        if (cap == maxCapacity && passengersOnboard.contains(p)) {
+            return false;
+
+        } else {
+            passengersOnboard.add(p);
+                return true;
+        }
         
 
     }
 
+    public Passenger removePassenger(Passenger p) {
+        pass;
+    }
+
+
+    /**
+     * prints out list of passengers on board. if no passengers present, will print out that car is empty.
+     * 
+     */
+
+    public void printManifest() {
+        System.out.println("Passengers on Board: ");
+        if (!passengersOnboard.isEmpty()) {          
+            System.out.println(passengersOnboard);
+        } else {
+            System.out.println("This car is EMPTY.");
+        }
+    }
 }
 

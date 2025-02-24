ba2b35e31571f2f992e546392fe7d930beb915bf
Victoria Reyes <victoriareyes@Victorias-MacBook-Air-2.local>
2025-02-23 19:54:22 -0500

    trainy

--- Train.java
+++ Train.java
@@ -1,3 +1,30 @@
-public class Train {
+import java.util.ArrayList;
+
+public class Train implements TrainRequirements{
+
+    //Attributes
+    private ArrayList <Car>  cars;
+    private Engine engine;
+
+
+   
+
+    /**
+     * @param fuelType
+     * @param currentFuelLevel
+     * @param fuelCapacity
+     * @param Car
+     * @param passengerCapacity
+     */
+    public Train(FuelType fuelType, double currentFuelLevel, double fuelCapacity, int nCar, int passengerCapacity){
+        
+        this.cars = new ArrayList<Car>();
+        nCar
+        this.engine = new Engine(fuelType, currentFuelLevel, fuelCapacity);
+
+
+
+    }
+
 
 }
